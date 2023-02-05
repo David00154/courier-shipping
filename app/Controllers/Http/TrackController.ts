2787d83a4e13f25f04f291f966ad0e4f44811745
david00154 <davidbriggs00154@gmail.com>
2023-02-06 00:38:50 +0100

    did track page

--- app/Controllers/Http/TrackController.ts
+++ app/Controllers/Http/TrackController.ts
@@ -1,8 +1,26 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class TrackController {
-  public async index({ view }: HttpContextContract) {
-    return await view.render("client/track");
+  public async index({
+    view,
+    request,
+    session,
+    response,
+  }: HttpContextContract) {
+    const { track_code } = request.only(["track_code"]);
+    let trackData: any = null;
+    if (track_code != undefined) {
+      //   console.log(track_code);
+      try {
+        trackData = {};
+      } catch (error) {
+        console.log(error);
+        session.flash("message.error", "Internal Server Error");
+      }
+    }
+    return await view.render("client/track", {
+      trackData,
+    });
   }
 
   //   public async show
